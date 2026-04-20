fc9e75c796b4d21cd9248e8a682cf109b0d53e84
Alex <oleksandr.kry@gmail.com>
2026-04-20 19:57:37 +0800

    Initial Commit

---
+++ weatherAnimation/weatherAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAnimationApp.swift
+//  weatherAnimation
+//
+//  Created by Alex on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct weatherAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
